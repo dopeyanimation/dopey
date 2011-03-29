e25736ede003567ea4520cf91f0d8d712db1870c
Manuel Quiñones <manuel.por.aca@gmail.com>
2011-03-29 11:17:16 -0300

    X Sheet list

--- gui/xsheetwindow.py
+++ gui/xsheetwindow.py
@@ -3,17 +3,24 @@ import gtk
 from gettext import gettext as _
 import gobject
 
-
 (
   COLUMN_FRAME_NUMBER,
   COLUMN_DESCRIPTION,
-  COLUMN_HAS_CEL
-) = range(3)
+  COLUMN_HAS_CEL,
+  COLUMN_FG_COLOR,
+  COLUMN_BG_COLOR,
+) = range(5)
 
 # test data:
 cel_list = []
 for i in range(1, 25):
-    cel_list.append([i, "", False])
+    cel_list.append({
+            'frame_number': i,
+            'description': "",
+            'has_cel': False,
+            'fg_color': '#222',
+            'bg_color': '#faa',
+    })
 
 
 class ToolWidget(gtk.VBox):
@@ -45,21 +52,12 @@ class ToolWidget(gtk.VBox):
         self.show_all()
     
     def __create_model(self):
-
-        lstore = gtk.ListStore(
-            gobject.TYPE_UINT,
-            gobject.TYPE_STRING,
-            gobject.TYPE_BOOLEAN
-            )
         
+        lstore = gtk.ListStore(int, str, bool, str, str)
         for cel in cel_list:
-            it = lstore.append()
-            lstore.set(it,
-                COLUMN_FRAME_NUMBER, cel[COLUMN_FRAME_NUMBER],
-                COLUMN_DESCRIPTION, cel[COLUMN_DESCRIPTION],
-                COLUMN_HAS_CEL, cel[COLUMN_HAS_CEL]
-            )                      
-        
+            lstore.append((cel['frame_number'], cel['description'],
+                           cel['has_cel'], cel['fg_color'],
+                           cel['bg_color']))
         return lstore
     
     def __add_columns(self, treeview):
@@ -67,26 +65,41 @@ class ToolWidget(gtk.VBox):
         model = treeview.get_model()
 
         # frame column
+        
         renderer = gtk.CellRendererText()
         renderer.set_data("column", COLUMN_FRAME_NUMBER)
-
-        column = gtk.TreeViewColumn(_("Frame"), renderer,
-                               text=COLUMN_FRAME_NUMBER)
-
-        # fix column to 50 pixels
+        # renderer.set_property('background-set' , True)
+        # renderer.set_property('foreground-set' , True)
+        
+        column = gtk.TreeViewColumn(_("Frame"))
+        column.pack_start(renderer, True)
+        column.set_attributes(renderer,
+                              text=COLUMN_FRAME_NUMBER, 
+                              # foreground=COLUMN_FG_COLOR,
+                              # background=COLUMN_BG_COLOR,
+                              )
+        
+        # fix column to 50 pixels:
         column.set_sizing(gtk.TREE_VIEW_COLUMN_FIXED)
         column.set_fixed_width(50)
-
+        
         treeview.append_column(column)
-
+        
         # description column
+        
         renderer = gtk.CellRendererText()
-        renderer.connect("edited", self.on_cell_edited, model)
         renderer.set_data("column", COLUMN_DESCRIPTION)
-
-        column = gtk.TreeViewColumn(_("Description"), renderer,
-                               text=COLUMN_DESCRIPTION,
-                               editable=False)
+        # renderer.set_property('background-set' , True)
+        # renderer.set_property('foreground-set' , True)
+        # renderer.set_property('background' , '#fff')
+        # renderer.set_property('foreground' , '#000')
+        renderer.set_property('editable' , True)
+        renderer.connect("edited", self.on_cell_edited, model)
+        
+        column = gtk.TreeViewColumn(_("Description"))
+        column.pack_start(renderer, True)
+        column.set_attributes(renderer, text=COLUMN_DESCRIPTION)
+        
         treeview.append_column(column)        
 
     def on_cell_edited(self, cell, path_string, new_text, model):
